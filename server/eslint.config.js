b46f9b414c3609cb2f61ea983f03b47a85fd87e6
Aditya Mishra <aditya.1165.mishra@gmail.com>
2026-04-27 15:30:00 +0530

    comprehensive fix for ci and linting

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -7,19 +7,30 @@ module.exports = [
             ecmaVersion: 2021,
             sourceType: "commonjs",
             globals: {
+                // Node.js globals
                 require: "readonly",
                 module: "readonly",
+                exports: "readonly",
                 process: "readonly",
                 __dirname: "readonly",
+                __filename: "readonly",
+                console: "readonly",
+                fetch: "readonly",
+                // Jest globals
                 describe: "readonly",
                 it: "readonly",
                 test: "readonly",
-                expect: "readonly"
+                expect: "readonly",
+                beforeEach: "readonly",
+                afterEach: "readonly",
+                beforeAll: "readonly",
+                afterAll: "readonly"
             }
         },
         rules: {
             "no-unused-vars": "warn",
-            "no-console": "off"
+            "no-console": "off",
+            "no-useless-escape": "off"
         }
     }
 ];
